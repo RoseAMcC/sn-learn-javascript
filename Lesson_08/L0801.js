6d53ea97835b9e897c580cb33b1da800d413a937
RoseAMcC <123596118+RoseAMcC@users.noreply.github.com>
2023-12-04 15:15:59 -0800

    Update L0801.js

--- Lesson_08/L0801.js
+++ Lesson_08/L0801.js
@@ -2,9 +2,17 @@
 //  L08S01 - Changing and detecting variable types
 //
 var i = 5;
+// turn something from a different type to a string
 var iStr = i.toString();
+// helps in troubleshooting
 gs.info(typeof i);
 gs.info(typeof iStr);
 
+//turn a string into a number
 var n = parseInt(iStr);
+
+//Adding strings is concatenation
 gs.info(typeof n + ' n=' + n);
+
+//to convert a string to floating point number
+var f= parseFloat(iStr)
