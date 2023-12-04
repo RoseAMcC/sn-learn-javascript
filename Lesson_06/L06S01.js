18c1c8c31e9f4633ea15c99230b3992b9edf531f
RoseAMcC <123596118+RoseAMcC@users.noreply.github.com>
2023-12-04 14:35:21 -0800

    Update L06S01.js

--- Lesson_06/L06S01.js
+++ Lesson_06/L06S01.js
@@ -3,9 +3,9 @@
 //
 var firstName = "Chuck";
 var lastName  = 'Tomasi';  // Either is ok
-var myCar     = "Chuck's Car";  // Mix them to avoid issues
+var myCar     = "Chuck's Car";  // Mix them to avoid issues Single Quotes inside Double Quotes will allow for apostrophes
 
 // Note the use of + when strings are involved
 var name = firstName + ' ' + lastName;
 gs.info(name);
-gs.info('length of name=' + name.length);
+gs.info('length of name=' + name.length);// very common in debugging. "length of name" before the = is a label for the value
