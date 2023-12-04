c314fc13e0486d765b54f113357e30257234d041
RoseAMcC <123596118+RoseAMcC@users.noreply.github.com>
2023-12-04 15:35:48 -0800

    Update L0801.js

--- Lesson_08/L0801.js
+++ Lesson_08/L0801.js
@@ -16,3 +16,18 @@ gs.info(typeof n + ' n=' + n);
 
 //to convert a string to floating point number
 var f= parseFloat(iStr)
+
+//LAB
+//define 3 strings of text
+var string1 = 'Chuck';
+var string2 = 'Tomasi';
+var string3 = 'JavaScript';
+//get the length of each string with a label of which string is measured
+gs.info('length1 = ' + string1.length);
+gs.info('length2 = ' + string2.length);
+gs.info('length3 = ' + string3.length);
+//concat all strings together with new line spaces and compare number of characters of strings vs. strings concat'd with newlines
+var allStrings = string1 + '\n' + string2 + '\n' + string3;
+gs.info('allStrings =' + allStrings);
+gs.info('length of allStrings =' + allStrings.length);
+//new lines will count as a character each
