b9a48672cce5f5bfaa2c7fa809f9a79d79f37805
RoseAMcC <123596118+RoseAMcC@users.noreply.github.com>
2023-12-04 16:03:15 -0800

    Update L11S01.js

--- Lesson_11/L11S01.js
+++ Lesson_11/L11S01.js
@@ -6,14 +6,24 @@ var b = 3;
 var c = 5;
 if (a < b)
   gs.info('a is less than b');
-
+//if there are more than one item that needs to happen after the if condition, it should be denoted with curly braces
+if (a < b) {
+  gs.info('a is less than b');
+  gs.info(Chuck was here);
+}  
+// for shortcutting to a true statement
+var bool = a < b
+if (bool) {
+  gs.info('a is less than b');
+  gs.info('Chuck was here');
+}
 // else
 if (a < b)
   gs.info('a is less than b');
 else
   gs.info('a is greater than or equal to b');
-
-// Else if and else
+// it is recommended to use curly braces for even single lines to make adding to the code easier later
+// Else if and else if for comparing multiple cases
 if (a < b)
   gs.info('a is less than b');
 else if (a > b)
