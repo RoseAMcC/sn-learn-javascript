2b25e6a1228ef9b58b80bb7a024d23045fab6163
RoseAMcC <123596118+RoseAMcC@users.noreply.github.com>
2023-12-07 11:35:19 -0800

    Update L16S01.js

--- Lesson_16/L16S01.js
+++ Lesson_16/L16S01.js
@@ -2,7 +2,16 @@
 // L16S01 - For loop
 //
 // Note: break and continue work here too!
-for (var i = 0; i < 5; i++) {
+for (var i = 0; i < 5; i++) {//DO NOT FORGET THE VAR PART, Continue and Break work the same here as in other loop statements
   gs.info(i);
 }
 gs.info('done i=' + i);
+/* produces
+*** Script: 0
+*** Script: 1
+*** Script: 2
+*** Script: 3
+*** Script: 4
+*** Script: done i=5
+*/
+
