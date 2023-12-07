25f8adc68269a2dc309d35d1f561abb138d52a86
RoseAMcC <123596118+RoseAMcC@users.noreply.github.com>
2023-12-07 11:02:31 -0800

    Update L15S01.js

--- Lesson_15/L15S01.js
+++ Lesson_15/L15S01.js
@@ -2,8 +2,18 @@
 // L15S01 - Simple while loop
 //
 var i = 0;
-while (i < 5) {
-  gs.info(i);
-  i++;
-}
+while (i < 5) { //until the value is = 5
+  gs.info(i); //print the value
+  i++; //increment the value
+} //need curly braces because there are two statements 
 gs.info('done i=' + i);
+
+/* produces the following:
+*** Script: 0
+*** Script: 1
+*** Script: 2
+*** Script: 3
+*** Script: 4
+*** Script: done i=5
+*/
+
