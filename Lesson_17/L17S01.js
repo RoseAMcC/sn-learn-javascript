ed1a6f659224c4a04e6bd5b86be5f48b1f18517d
RoseAMcC <123596118+RoseAMcC@users.noreply.github.com>
2023-12-07 11:40:58 -0800

    Update L17S01.js

--- Lesson_17/L17S01.js
+++ Lesson_17/L17S01.js
@@ -1,10 +1,22 @@
 //
 // L17S01 - do-while
-//
+//the critical factor here is the loop runs at least once
+// Use any time you want the code to run at least once
+// Anything that has something outside the loop asking if the loops needs to run is a candidate for a do while loop.
 var i = 0;
 gs.info('start');
-do {
+do { 
   gs.info('i=' + i);
   ++i;
 } while (i < 5);
 gs.info('done i=' + i);
+/*
+produces
+*** Script: start
+*** Script: i=0
+*** Script: i=1
+*** Script: i=2
+*** Script: i=3
+*** Script: i=4
+*** Script: done i=5
+*/
