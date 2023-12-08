19e027555a968443b335bd7721a3caac8198b95e
RoseAMcC <123596118+RoseAMcC@users.noreply.github.com>
2023-12-08 11:35:20 -0800

    Update L19S02.js

--- Lesson_19/L19S02.js
+++ Lesson_19/L19S02.js
@@ -1,11 +1,19 @@
 //
 // L19S02 - Function with a parameter
 //
-function toCelsius(fahrenheit) {
+function toCelsius(fahrenheit) { //Parameter is fahrenheit
 
-    var c = (5 / 9) * (fahrenheit - 32);
+    var c = (5 / 9) * (fahrenheit - 32);  // this is the calculation. var c is a floating point number with at conversion formula from the internet
 
-    gs.info(c);
+    gs.info(c); // this is the output and it happens at the same time as the calculation
 }
-toCelsius(32);
-toCelsius(100);
+toCelsius(32); // we want to run this function twice, once with a fahrenheit temp of 32
+toCelsius(100); // then we run the function again with the fahrenheit temp of 100
+
+/*
+Produces
+*** Script: 0
+*** Script: 37.77777777777778
+*/
+
+
