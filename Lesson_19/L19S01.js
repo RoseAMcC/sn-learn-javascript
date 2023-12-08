e925b4cd367a11d27d84b0119d876aebfc108187
RoseAMcC <123596118+RoseAMcC@users.noreply.github.com>
2023-12-08 11:28:28 -0800

    Update L19S01.js

--- Lesson_19/L19S01.js
+++ Lesson_19/L19S01.js
@@ -1,7 +1,7 @@
 //
 // L19S01 - Functions
 //
-function sayHello() {
+function sayHello() { //NEVER NAME YOUR VARIABLE FUNCTION, because that is used for functions already
   gs.info('Hello');
 }
 sayHello();
