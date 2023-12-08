749365dfca7f76de42372c32488f6a8024fd410b
RoseAMcC <123596118+RoseAMcC@users.noreply.github.com>
2023-12-08 11:38:32 -0800

    Update L19S03.js

--- Lesson_19/L19S03.js
+++ Lesson_19/L19S03.js
@@ -3,10 +3,17 @@
 //
 function toCelsius(fahrenheit) {
 
-    return (5 / 9) * (fahrenheit - 32);
+    return (5 / 9) * (fahrenheit - 32); // if we want to run a calculation and get a single value out of function
 
 }
-var c = toCelsius(32);
+var c = toCelsius(32); // we call the function to run for the fahrenheight value '32', and now we are storing in variable var c
 gs.info(c);
-c = toCelsius(212);
+c = toCelsius(212); // then we take variable c and rework it for fahrenheight value '212'
 gs.info(c);
+
+/*
+Produces
+*** Script: 0
+*** Script: 100
+*/
+
